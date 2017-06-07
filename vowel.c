7ab787eb62dbba506f5e370f14f2b6c17422c43c
suryaeee <suryarajendran96@gmail.com>
2017-06-07 11:04:38 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+int lowervowel,uppervowel;
+scanf("%c",&c);
+lowervowel=(c==a||c==e||c==i||c==o||c==u);
+uppervowel=(c==A||c==E||c==I||c==O||c==U);
+if(lowervowel==upperlevel)
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is consonant");
+}
+getch();
+}
