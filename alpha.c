0f5b797b032d38af304d69a0bac66ce51879b57d
suryaeee <suryarajendran96@gmail.com>
2017-06-07 11:20:22 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char ch;
+scanf("%c",&c);
+if(c>==a&&c<==z)||(c>==A&&c<==Z)
+{
+printf("it is alphabet");
+}
+else
+{
+printf("it is not an alphabet");
+}
+return0;
+getch();
+}
